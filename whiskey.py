f569f48a69b2931137ccfb5ad7086d7260374f7d
John <swrath@hotmail.com>
2019-08-12 12:58:09 +0800

    whiskey project

--- whiskey.py
+++ whiskey.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.cluster.bicluster import SpectralCoclustering
+import matplotlib.pyplot as plt
 
 whisky = pd.read_csv("whiskies.txt")
 whisky["Region"] = pd.read_csv("regions.txt")
@@ -27,3 +29,29 @@ plt.colorbar()
 # >>>plt.savefig("corlate-whisky2.pdf")
 
 plt.show()
+
+model = SpectralCoclustering(n_clusters=6, random_state=0)
+model.fit(corr_whisky)
+model.rows_
+     #>>>np.sum(model.rows_, axis=1)
+     #>>>np.sum(model.rows_, axis=0)
+model.row_labels_
+
+whisky['Group'] = pd.Series(model.row_labels_, index=whisky.index)
+whisky = whisky.ix[np.argsort(model.row_labels_)]
+whisky = whisky.reset_index(drop=True)
+
+correlations = pd.DataFrame.corr(whisky.iloc[:, 2:14].transpose())
+correlations = np.array(correlations)
+
+plt.figure(figsize=(14, 7))
+plt.subplot(121)
+plt.pcolor(corr_whisky)
+plt.title("Original")
+plt.axis("tight")
+plt.subplot(122)
+plt.pcolor(correlations)
+plt.title("Rearranged")
+plt.axis("tight")
+plt.show()
+plt.savefig("correlations.pdf")
